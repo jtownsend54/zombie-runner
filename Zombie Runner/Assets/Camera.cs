18b879de98fe316cde58c164c8de35531da3d10d
Jon Townsend <jon.townsend@limelyte.com>
2018-04-05 21:02:57 -0700

    Initial terrain
    River and bordering hills

---
+++ Zombie Runner/Assets/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+	Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+		rotation = new Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotation.y += 0.1f;
+		transform.rotation = Quaternion.Euler(rotation);
+	}
+}
