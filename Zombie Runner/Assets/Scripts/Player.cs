3e41315064a7ddbeb34a384768448c77ac15c7a5
Jon Townsend <jon.townsend@limelyte.com>
2018-04-13 06:56:32 -0700

    Added spawn points
    *Player now randomly spawns when the game starts.
    *Player script has a checkbox that allows to do a respawn during the game.

---
+++ Zombie Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public GameObject spawnPointContainer;
+	public bool doSpawn;
+
+	private SpawnPoint[] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+		ReSpawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (doSpawn) {
+			ReSpawn();
+		}
+
+		doSpawn = false;
+	}
+
+	void ReSpawn() {
+		spawnPoints = spawnPointContainer.GetComponentsInChildren<SpawnPoint> ();
+
+		int randomIdx = Random.Range(0, spawnPoints.Length);
+
+		GetComponent<Transform>().position = spawnPoints[randomIdx].GetComponent<Transform>().position;
+	}
+}
