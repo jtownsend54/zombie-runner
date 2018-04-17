24b4f40871a51147263c4cb6275826708d832555
Jon Townsend <jon.townsend@limelyte.com>
2018-04-16 21:22:02 -0700

    Input mapping
    * Added the asset start pack
    * Added zoom key for player
    * Added Call Helo key and helicopter script

---
+++ Zombie Runner/Assets/Scripts/Eyes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+	private Camera camera;
+	private float playerFOV;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera> ();
+		playerFOV = camera.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Input.GetAxis ("Zoom"));
+		if (Input.GetAxis ("Zoom") != 0f) {
+			camera.fieldOfView = playerFOV - (playerFOV * .5f);
+		} else {
+			camera.fieldOfView = playerFOV;
+		}
+	}
+}
