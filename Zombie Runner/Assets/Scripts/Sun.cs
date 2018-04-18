fd7943170d5b5ec6ac38d67fac09b2ea26d7b3d3
Jon Townsend <jon.townsend@limelyte.com>
2018-04-18 06:45:13 -0700

    Rotate the sun around scene.

---
+++ Zombie Runner/Assets/Scripts/Sun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+	// Minutes per second
+	public float timeScale;
+
+	private Vector3 rotation;
+
+	// Use this for initialization
+	void Start () {
+		rotation = new Vector3 (timeScale / 3600, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotation);
+	}
+}
